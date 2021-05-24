dde4b6ed5162caf0d62a76903eed496bfdc09491
AdnanM7 <71844402+AdnanM7@users.noreply.github.com>
2021-05-24 15:48:56 +0530

    Add files via upload

---
+++ EYHAC/EYHAC.ino
@@ -0,0 +1,168 @@
+                            /*
+                            *
+                            * Project Name:   Multifunctional Automatic Dispenser
+                            * Author Name:    Adnan J Mujawar
+                            * Mentored By:    Prof. Dr. Prajakta B Kanitkar   
+                            * Institute:      P.E.S. Modern College of Engineering
+                            * Filename:     EYHAC
+                            * 
+                            *
+                            */
+
+
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+#include <Adafruit_MLX90614.h>
+
+//initialise buzzer at pin 4
+int tone = 4;
+//const byte bt = 0x5A;
+//const byte spo = 0x57; 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); //Declaring the display name (display)
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+MAX30105 particleSensor;
+
+
+#define MAX_BRIGHTNESS 255
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+#endif
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+byte pulseLED = 11; //Must be on PWM pin
+byte readLED = 13; //Blinks with each data read
+
+//#define SCREEN_WIDTH 128 // OLED display width, in pixels
+//#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+//#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); //Declaring the display name (display)
+
+static const unsigned char PROGMEM logo2_bmp[] =
+{ 0x03, 0xC0, 0xF0, 0x06, 0x71, 0x8C, 0x0C, 0x1B, 0x06, 0x18, 0x0E, 0x02, 0x10, 0x0C, 0x03, 0x10,              //Logo2 and Logo3 are two bmp pictures that display on the OLED if called
+0x04, 0x01, 0x10, 0x04, 0x01, 0x10, 0x40, 0x01, 0x10, 0x40, 0x01, 0x10, 0xC0, 0x03, 0x08, 0x88,
+0x02, 0x08, 0xB8, 0x04, 0xFF, 0x37, 0x08, 0x01, 0x30, 0x18, 0x01, 0x90, 0x30, 0x00, 0xC0, 0x60,
+0x00, 0x60, 0xC0, 0x00, 0x31, 0x80, 0x00, 0x1B, 0x00, 0x00, 0x0E, 0x00, 0x00, 0x04, 0x00,  };
+
+static const unsigned char PROGMEM logo3_bmp[] =
+{ 0x01, 0xF0, 0x0F, 0x80, 0x06, 0x1C, 0x38, 0x60, 0x18, 0x06, 0x60, 0x18, 0x10, 0x01, 0x80, 0x08,
+0x20, 0x01, 0x80, 0x04, 0x40, 0x00, 0x00, 0x02, 0x40, 0x00, 0x00, 0x02, 0xC0, 0x00, 0x08, 0x03,
+0x80, 0x00, 0x08, 0x01, 0x80, 0x00, 0x18, 0x01, 0x80, 0x00, 0x1C, 0x01, 0x80, 0x00, 0x14, 0x00,
+0x80, 0x00, 0x14, 0x00, 0x80, 0x00, 0x14, 0x00, 0x40, 0x10, 0x12, 0x00, 0x40, 0x10, 0x12, 0x00,
+0x7E, 0x1F, 0x23, 0xFE, 0x03, 0x31, 0xA0, 0x04, 0x01, 0xA0, 0xA0, 0x0C, 0x00, 0xA0, 0xA0, 0x08,
+0x00, 0x60, 0xE0, 0x10, 0x00, 0x20, 0x60, 0x20, 0x06, 0x00, 0x40, 0x60, 0x03, 0x00, 0x40, 0xC0,
+0x01, 0x80, 0x01, 0x80, 0x00, 0xC0, 0x03, 0x00, 0x00, 0x60, 0x06, 0x00, 0x00, 0x30, 0x0C, 0x00,
+0x00, 0x08, 0x10, 0x00, 0x00, 0x06, 0x60, 0x00, 0x00, 0x03, 0xC0, 0x00, 0x00, 0x01, 0x80, 0x00  };
+
+void setup() {  
+  mlx.begin(); 
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //Start the OLED display
+  display.clearDisplay();
+  display.display();
+  pinMode (tone, OUTPUT);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+  particleSensor.begin(Wire, I2C_SPEED_FAST);
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+}
+void loop() {
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+  
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+    while (1)
+    { 
+     //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+      for (byte i = 25; i < 100; i++)
+      {
+        redBuffer[i - 25] = redBuffer[i];
+        irBuffer[i - 25] = irBuffer[i];
+      }
+
+     //take 25 sets of samples before calculating the heart rate.
+      for (byte i = 75; i < 100; i++)
+      {
+        while (particleSensor.available() == false) //do we have new data?
+          particleSensor.check(); //Check the sensor for new data
+
+        digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+        redBuffer[i] = particleSensor.getRed();
+        irBuffer[i] = particleSensor.getIR();
+        particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+  
+        //display spo2 levels on oled display
+        display.clearDisplay();  
+        display.setTextSize(1);
+        display.setTextColor(WHITE);
+        display.setCursor(0,0);
+        display.println("SpO2");
+        display.setTextSize(1);
+        display.setCursor(0,10);
+        display.println(spo2);
+        display.display();
+        
+        //body temperature display on oled display
+        display.setTextSize(1);                    
+        display.setTextColor(WHITE);             
+        display.setCursor(50,0);                
+        display.println("BODY TEMP:"); 
+ 
+  
+        display.setTextSize(1);
+        display.setCursor(50,10);
+        display.println(mlx.readObjectTempF(),1);
+  
+        display.setCursor(80,10);
+        display.println("F"); 
+        display.display();
+        //if body temperature of human passes 100*F notify danger
+        if (mlx.readObjectTempF() >= 100){
+          delay(1000);
+          digitalWrite (tone, HIGH);
+          digitalWrite(LED_BUILTIN, HIGH);
+          delay(5000);
+          digitalWrite (tone, LOW);
+          digitalWrite(LED_BUILTIN, LOW);
+        }
+      
+
+  
+}
+      maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+          
+        
+        }
+
+  }
+        
+  
